22f9b4447fb5896f1830bd16181f52f9acc0085d
elcynia <elysiabyss@gmail.com>
2025-07-23 19:55:16 +0900

    feat: add GA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    VitePluginRadar({
+      analytics: process.env.VITE_GA_ID ? { id: process.env.VITE_GA_ID } : undefined,
+    }),
+    react(),
+    tailwindcss(),
+  ],
 });
