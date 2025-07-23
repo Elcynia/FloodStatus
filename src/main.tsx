b21193e1e00f443b70caabe34d1ea0c74944c002
elcynia <elysiabyss@gmail.com>
2025-07-23 23:31:48 +0900

    feat: add browserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { Analytics } from '@vercel/analytics/react';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Analytics />
+    <BrowserRouter>
+      <App />
+      <Analytics />
+    </BrowserRouter>
   </StrictMode>
 );
