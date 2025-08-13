d994c24303e361d7fb749c743992e864b51eb3a2
elcynia <elysiabyss@gmail.com>
2025-08-13 18:36:47 +0900

    chore(seo): add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Helmet } from 'react-helmet';
 import FloodMap from './components/FloodMap';
 
 function App() {
-  return <FloodMap />;
+  return (
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>하천 수위 상황판</title>
+        <link rel='canonical' href='https://floodstatus.vercel.app/' />
+      </Helmet>
+      <FloodMap />
+    </>
+  );
 }
 
 export default App;
