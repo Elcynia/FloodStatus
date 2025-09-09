027c261bc507e7b67f48d3272bdf52dbec017a3b
elcynia <elysiabyss@gmail.com>
2025-09-09 15:42:26 +0900

    seo: add helmet title

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <>
       <Helmet>
+        <title>서울시 하천 수위 상황판</title>
+        <meta property='og:title' content='서울시 하천 수위 상황판' />
         <meta charSet='utf-8' />
-        <title>하천 수위 상황판</title>
         <link rel='canonical' href='https://floodstatus.vercel.app/' />
       </Helmet>
       <FloodMap />
