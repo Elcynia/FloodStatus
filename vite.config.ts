14e49153630de26ef70c45aebe141452b10ac5b9
elcynia <elysiabyss@gmail.com>
2025-10-30 16:00:49 +0900

    seo: add sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import { VitePluginRadar } from 'vite-plugin-radar';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    Sitemap({ hostname: 'https://floodstatus.vercel.app' }),
   ],
 });
